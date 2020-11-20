b81d1b4f484a27611d12e65a78f8f86a88ab4681
Nicholas Browdues <browdues@gmail.com>
2020-11-19 17:50:00 -0700

    add edge case: free movie card

--- cinemaSystems/movie.go
+++ cinemaSystems/movie.go
@@ -1,6 +1,20 @@
 package movie
 
-// Movie returns
+import (
+	"math"
+)
+
+// Movie returns the first n where ceil(systemB) < ceil(systemA)
 func Movie(card, ticket int, perc float64) int {
-	return 0
+	systemA := float64(ticket)
+	systemB := float64(card) + perc*float64(ticket)
+	n := 1
+
+	for systemA <= math.Ceil(systemB) {
+		n++
+		systemA += float64(ticket)
+		systemB += math.Pow(perc, float64(n)) * float64(ticket)
+	}
+
+	return n
 }
--- cinemaSystems/movie_test.go
+++ cinemaSystems/movie_test.go
@@ -6,7 +6,17 @@ import (
 	. "github.com/stretchr/testify/assert"
 )
 
-func TestAddWithTestify(t *testing.T) {
-	n := Movie(100, 10, .1)
-	Equal(t, 0, n)
+func TestMovieExample1(t *testing.T) {
+	n := Movie(500, 15, 0.9)
+	Equal(t, 43, n)
+}
+
+func TestMovieExample2(t *testing.T) {
+	n := Movie(100, 10, 0.95)
+	Equal(t, 24, n)
+}
+
+func TestMovieFreeCard(t *testing.T) {
+	n := Movie(0, 10, 0.95)
+	Equal(t, 2, n)
 }
