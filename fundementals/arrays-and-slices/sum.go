35c2e45001ed4de07e62e3751a97e66ff10f895c
Nicholas Browdues <browdues@gmail.com>
2020-11-21 06:52:14 -0700

    fundementals reveiew: slices

--- fundementals/arrays-and-slices/sum.go
+++ fundementals/arrays-and-slices/sum.go
@@ -1,6 +1,38 @@
 package sum
 
 // Sum returns the sum of the array
-func Sum(numbers [5]int) int {
-	return 0
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+// All takes n slices and returns n sums
+func All(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numberSlice := range numbersToSum {
+		sums = append(sums, Sum(numberSlice))
+	}
+
+	return sums
+}
+
+// AllTails takes n slices and returns the n tail sums
+// a tail sum is the sum of all numbers in a slice except the first (its head)
+func AllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, sumSlice := range numbersToSum {
+		if len(sumSlice) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := sumSlice[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
--- fundementals/arrays-and-slices/sum_test.go
+++ fundementals/arrays-and-slices/sum_test.go
@@ -1,16 +1,63 @@
 package sum
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	numbers := [...]int{1, 2, 3, 4, 5} // fixed capacity
+	t.Run("sums any size collection", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	want := 15
-	got := Sum(numbers)
+		want := 6
+		got := Sum(numbers)
 
-	if got != want {
-		t.Errorf("got %v want %d given, %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got %v want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sums any size slice collection", func(t *testing.T) {
+		numbersA := []int{1, 2, 3}
+		numbersB := []int{1, 1, 1}
+
+		want := []int{6, 3}
+		got := All(numbersA, numbersB)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %d", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
 	}
+
+	t.Run("make sums of some slices", func(t *testing.T) {
+		numbersA := []int{1, 2, 3}
+		numbersB := []int{1, 1, 1}
+
+		want := []int{5, 2}
+		got := AllTails(numbersA, numbersB)
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum empty slices", func(t *testing.T) {
+		numbersA := []int{}
+		numbersB := []int{1, 1, 1}
+
+		want := []int{0, 2}
+		got := AllTails(numbersA, numbersB)
+
+		checkSums(t, got, want)
+	})
 }
