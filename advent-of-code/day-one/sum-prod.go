42a10ad874a493b107d6e4809a5bf16565ebda4b
Nicholas Browdues <browdues@gmail.com>
2020-12-11 15:16:31 -0700

    day 1

--- advent-of-code/day-one/sum-prod.go
+++ advent-of-code/day-one/sum-prod.go
@@ -2,11 +2,56 @@ package sumprod
 
 import (
 	"bufio"
+	"errors"
 	"os"
+	"strconv"
+	"strings"
 )
 
+// Find2020Sum takes a list of ints and checks if any two sum to 2020
+func Find2020Sum(numbers []int64) ([]int64, error) {
+	attempted := make([]int64, 0)
+	for _, num := range numbers {
+		if num > 2020 {
+			continue
+		}
+
+		for _, attempt := range attempted {
+			if num+attempt == 2020 {
+				return []int64{num, attempt}, nil
+			}
+		}
+		attempted = append(attempted, num)
+	}
+	return nil, errors.New("Couldn't find two numbers that sum to 2020")
+}
+
+// Find2020TripleSum takes a list of ints and checks if any three sum to 2020
+func Find2020TripleSum(numbers []int64) ([]int64, error) {
+	firstAttempt := make([]int64, 0)
+	secondAttempt := make([]int64, 0)
+
+	for _, num1 := range numbers {
+		if num1 > 2020 {
+			continue
+		}
+
+		for _, num2 := range firstAttempt {
+			for _, num3 := range secondAttempt {
+				if num1+num2+num3 == 2020 {
+					return []int64{num1, num2, num3}, nil
+				}
+			}
+			secondAttempt = append(secondAttempt, num2)
+		}
+
+		firstAttempt = append(firstAttempt, num1)
+	}
+	return nil, errors.New("Couldn't find two numbers that sum to 2020")
+}
+
 // LinesToArray accepts a path to a text file and returns an array where each element is a line of the txt file.
-func LinesToArray(fpath string) ([]string, error) {
+func LinesToArray(fpath string) ([]int64, error) {
 
 	file, err := os.Open(fpath)
 	if err != nil {
@@ -19,10 +64,16 @@ func LinesToArray(fpath string) ([]string, error) {
 
 	scanner.Split(bufio.ScanLines)
 
-	var lines []string
+	var lines []int64
 
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		line := strings.Split(scanner.Text(), " ")[0]
+		intLine, err := strconv.ParseInt(line, 10, 64)
+
+		if err != nil {
+			return nil, err
+		}
+		lines = append(lines, intLine)
 	}
 
 	return lines, nil
--- advent-of-code/day-one/sum-prod_test.go
+++ advent-of-code/day-one/sum-prod_test.go
@@ -1,17 +1,40 @@
 package sumprod
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFixture(t *testing.T) {
+func TestReadLines(t *testing.T) {
 	t.Run("can read lines from a text file", func(t *testing.T) {
 		fpath := "./input.txt"
 		lines, err := LinesToArray(fpath)
 
-		if err != nil {
-			fmt.Println("unexpected err")
-		}
+		assert.NoError(t, err)
+		assert.Equal(t, 200, len(lines))
+	})
+}
+
+var (
+	fpath    = "./input.txt"
+	lines, _ = LinesToArray(fpath)
+)
+
+func TestFind2020Sum(t *testing.T) {
+	t.Run("find two numbers that sum to 2020", func(t *testing.T) {
+		s, err := Find2020Sum(lines)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(s))
+		assert.Equal(t, int64(2020), s[0]+s[1])
+	})
+
+	t.Run("find three numbers that sum to 2020", func(t *testing.T) {
+		s, err := Find2020TripleSum(lines)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 3, len(s))
+		assert.Equal(t, int64(2020), s[0]+s[1]+s[2])
 	})
 }
